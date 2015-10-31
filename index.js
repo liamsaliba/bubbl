b4a62ab2f671c73d466e88087cfc1584a1f58502
lsp0 <liamsaliba@hotmail.com>
2015-10-31 13:37:27 +1100

    Initial Commit (0.12)

---
+++ index.js
@@ -0,0 +1,115 @@
+// function handler : express
+var express = require('express');
+var app = express();
+// http server made from function handler 'app'
+var http = require('http').Server(app);
+// socket io
+var io = require('socket.io')(http);
+
+// Route handler, sends index.html when root address is hit.
+app.use(express.static(__dirname + '/public'));
+
+// holds usernames
+var usernames = {};
+var users_connected = 0;
+
+// socket.io: user connection
+io.on('connection', function(socket){  // listening socket
+
+	// if user has a username and is in the chat room.
+	var userJoined = false;
+
+	// emit chat message with username and message to other clients
+	socket.on('chat message', function(data){
+		socket.broadcast.emit('chat message', {
+			username: socket.username,
+			colour: socket.colour,
+			message: data
+		});
+	});
+
+	socket.on('join', function(){
+		if(typeof usernames[socket.id] !== "")
+		socket.username = genUser(); //store username in socket session for client
+		socket.colour = 280;
+		usernames[socket.id] = {username: socket.username, colour: socket.colour, status: "online"}; // store username in temporary db
+		print("+ " + socket.id + " : " + usernames[socket.id].username + " : h" + usernames[socket.id].colour);
+		++users_connected;
+
+		userJoined = true;
+
+		io.to(socket.id).emit("setname", socket.username)
+		io.to(socket.id).emit("update names", {
+			usernames: usernames,
+			numUsers: users_connected
+		});
+
+		socket.broadcast.emit('join', {
+			username: socket.username,
+			numUsers: users_connected
+		});
+	});
+
+	socket.on('leave', function(){
+		print("- " + socket.id + " : " + usernames[socket.id].username + " : h" + usernames[socket.id].colour);
+		if(userJoined){
+			delete usernames[socket.id]
+
+			--users_connected;
+
+			socket.broadcast.emit('leave', {
+				username: socket.username,
+				numUsers: users_connected
+			});
+		}
+	});
+
+	// force refresh
+	socket.on('fr', function(){
+		io.emit('fr');
+	});
+
+	socket.on('away', function(){
+		socket.broadcast.emit('away', socket.username);
+	});
+
+	socket.on('back', function(){
+		socket.broadcast.emit('back', socket.username);
+	});
+
+	socket.on('change colour', function(colour) {
+		socket.colour = colour;
+		usernames[socket.id].colour = colour;
+		print("! " + socket.id + " : " + usernames[socket.id].username + " : h" + usernames[socket.id].colour);
+	});
+
+	socket.on('typing', function(msg){
+		//socket.broadcast.emit('back', socket.username); //TODO
+	});
+
+	socket.on('typing stop', function(){
+
+	});
+});
+
+// HTTP server, listen for activity on port 3000
+http.listen(8080, function(){
+	print("Server initialised.")
+	print("Listening on *:80")
+});
+
+function print(str){
+	var d = new Date();
+	console.log(d.toISOString() + ' | ' + str);
+}
+
+// generate username : TODO
+function genUser() {
+	var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    var name = [];
+    for(var i = 0; i < (Math.ceil(Math.random()*7) + 4); i++)
+    	name.push(possible.charAt(Math.floor(Math.random() * possible.length)));
+
+	return name.join('');
+}
---
+++ public/assets/main.js
@@ -0,0 +1,352 @@
+var socket = io();
+socket.emit('join');
+
+var typing = false; //TODO
+
+// set username
+var user_hue = 280;
+var username;
+var prev_msg_username;
+var prev_msg;
+
+// bubbl logo
+var bubbl = "<object data=\"/assets/bubbl.svg\" type=\"image/svg+xml\" height=\"10px\" style=\"padding-left: 2px;\"></object>";
+
+var colours = {}; // these are defined in the colour section.
+
+
+// fix padding for messages
+//$("#msgs").css('padding-top', (window.innerHeight-100));
+//$(window).resize(function(){
+//	$("#msgs").css('padding-top', (window.innerHeight-100));
+//});
+
+
+// Talking TO server
+msg('n d', "you joined " + bubbl);
+
+change_color(280); // maybe use a cookie to change colour?
+
+// send leave receipt to server
+window.onbeforeunload = function() { 
+	socket.emit('leave');
+} 
+
+// TODO
+$(window).blur(function(){
+	socket.emit('away');
+});
+$(window).focus(function(){
+  	socket.emit('back');
+});
+
+$('#logo').click(function(){
+	$('#logo').animate({height: '30px'}, "fast");
+	if($('#float-panel').css('display') === 'none')
+		$('#float-panel').stop(true, true).fadeIn({ duration: 400, queue: false }).css('display', 'none').slideDown(400);
+	else
+    	$('#float-panel').stop(true, true).fadeOut({ duration: 400, queue: false }).slideUp(400);
+    $('#logo').animate({height: '35px'}, "fast");
+});
+
+$('.u').click(function(){
+	$("form input").append($(this).text());
+});
+
+//		<span id="change_tip">change username -></span>
+
+$('#float-username').click(function(){
+	$('#float-username').animate({right: 0}, "slow");
+	$('#users li:contains(' + username + ')').remove();
+	socket.emit("leave");
+	socket.emit("join");
+});
+
+// Chat box parsing
+$('form').submit(function(){
+	// remove white space, make html safe.
+	var m = fix($('#m').val().trim());
+
+	// ! commands
+	if (m.charAt(0) === "!"){
+		prev_msg_username = false;
+
+		if(m === "!clear")
+			$("#messages").empty();
+		// !h
+		else if ((m.charAt(1) === "h" && m.substring(2) === " ") && !isNaN(m.substring(3))){
+			console.log(parseInt(m.substring(3)));
+
+			change_color(m.substring(3));
+			msg('n d', "successfully changed your hue to " + m.substring(3));
+		} // !c
+		else if (m.charAt(1) === "c" && m.substring(3) !== ""){
+			switch (m.substring(3)){
+				case 'purple':
+				case 'reset':
+				case 'default':
+					change_color(280);
+					msg('n d', "successfully changed your colour to purple");
+					break;
+				case 'red':
+					change_color(0);
+					msg('n d', "successfully changed your colour to red");
+					break;
+				case 'orange':
+					change_color(25);
+					msg('n d', "successfully changed your colour to orange");
+					break;
+				case 'gold':
+					change_color(45);
+					msg('n d', "successfully changed your colour to gold");
+					break;
+				case 'yellow':
+					change_color(60);
+					msg('n d', "successfully changed your colour to yellow");
+					break;
+				case 'lime':
+					change_color(80);
+					msg('n d', "successfully changed your colour to lime");
+					break;
+				case 'green':
+					change_color(100);
+					msg('n d', "successfully changed your colour to green");
+					break;
+				case 'turquoise':
+					change_color(150);
+					msg('n d', "successfully changed your colour to turquoise");
+					break;
+				case 'aqua': 
+					change_color(175);
+					msg('n d', "successfully changed your colour to aqua");
+					break;
+				case 'sky':
+					change_color(200);
+					msg('n d', "successfully changed your colour to sky");
+					break;
+				case 'blue':
+					change_color(240);
+					msg('n d', "successfully changed your colour to blue");
+					break;
+				case 'magenta':
+					change_color(310);
+					msg('n d', "successfully changed your colour to magenta");
+					break;
+				case 'pink':
+					change_color(330);
+					msg('n d', "successfully changed your colour to pink");
+					break;
+				default:
+					msg('n e', "invalid colour. try a number or colour name.");
+						break;
+			}
+			
+		}
+		else if (m.substring(1,3) === "fr"){
+			socket.emit('fr');
+		}
+		else
+			msg('n e', "invalid command. use \\ to escape ! commands.");
+	} 
+	// escape \
+	else if (m.charAt(0) === "\\") 
+		send_msg(m.substring(1));
+	else
+		send_msg(m);
+	
+	window.scrollBy(0, 200000);
+
+	// reset box
+	$('#m').val('');
+	return false;
+});
+
+
+
+// Socket recieve responses
+socket.on('chat message', function(data){
+	msgm(data.message, data.username, data.colour);
+});
+
+socket.on('update names', function(data){
+	$('#users').empty();
+	$('#users').append($('<li class="user">').html(username + "  <span class=\"online\">&#9679;</span>"));
+	for(var key in data.usernames){
+		if(data.usernames[key].username !== username){
+			if(data.usernames[key].status === "online")
+				$('#users').append($('<li>').html(data.usernames[key].username + "  <span class=\"online\">&#9679;</span>"));
+			else if(data.usernames[key].status === "away")
+				$('#users').append($('<li>').html(data.usernames[key].username + "  <span class=\"away\">&#9679;</span>"));
+		}
+	}
+	$('#online-count').html(data.numUsers + " online");
+});
+
+socket.on('join', function(data){
+	msg('n j', data.username + " joined " + bubbl);
+	$('#users').append($('<li>').html(data.username + "  <span class=\"online\">&#9679;</span>"));
+	$('#online-count').html(data.numUsers + " online");
+});
+
+socket.on('leave', function(data){
+	msg('n l', data.username + " left " + bubbl);
+	$('#users li:contains(' + data.username + ')').remove();
+	$('#online-count').html(data.numUsers + " online");
+});
+
+socket.on('fr', function(){
+	location.reload(true);
+});
+
+socket.on('setname', function(m){
+	username = m;
+	$('#float-username').animate({right: '130px'}, "slow");
+	$('#float-username').html(username);
+	document.title = 'bubbl. ' + username;
+});
+
+// message into client
+function msg(id, m){	  			
+	var offset = document.body.offsetHeight;
+
+	$('#messages').append($('<li>').append($('<div class="' + id + '">').html(m)));
+
+	if(isEnabled(id)) //disable
+		$('#messages div:last').fadeIn(300);
+
+	scroll(offset);	
+
+	prev_msg_username = false;
+}
+// replies
+function msgm(m, user, hue){
+	var offset = document.body.offsetHeight;
+
+	if (~m.indexOf(username)){
+		$('#messages').append($('<li>').append($('<div class="u">').html(user + '<span class="invisible">: </span>')).append($('<div class="i">').html(m)));
+		if(prev_msg_username === user)
+			$(".i:last, .u:last").addClass("same");
+		$('.i:last').css("background-color", colours.eighty).css("color", idealTextColor(colours.eighty)).fadeIn(300);
+	}
+	else {
+		$('#messages').append($('<li>').append($('<div class="u">').html(user + '<span class="invisible">: </span>')).append($('<div class="m">').html(m)));
+		if(prev_msg_username === user)
+			$(".m:last, .u:last").addClass("same");
+		$('.m:last').css("background-color", "hsl(" + hue + ", 100%, 95%").css("color", idealTextColor("hsl(" + hue + ", 100%, 95%)")).fadeIn(300);
+	}
+
+	$('.u:last').fadeIn(300);
+
+	prev_msg_username = user;
+
+	scroll(offset);
+}
+
+// message to server
+function send_msg(m){
+	if (is_legal(m)) {
+		socket.emit('chat message', m);
+		$('#messages').append($('<li>').append($('<div class="u user">').html(username + '<span class="invisible">: </span>')).append($('<div class="o">').html(m)))
+		if(prev_msg_username === username)
+			$(".u:last, .o:last").addClass("same");
+		$('.o:last').css("background-color", colours.fifty).css("color", idealTextColor(colours.fifty)).fadeIn(300).fadeIn(300);
+		$('.u:last').fadeIn(300);
+		prev_msg_username = username;
+	}
+	else 
+		msg('n e', "do not use invalid characters.")			
+}
+
+// TODO: spam protection in this.
+function is_legal(m){
+	if (m === "") // if nothing entered
+		return false;
+	else
+		return true;
+}
+
+function isEnabled(id){
+	switch (id) {
+		case "n j":
+			return false;
+		case "n l":
+			return false;
+		default:
+			return true;
+	}
+}
+
+// Autoscroll
+function scroll(offset){
+	if((window.innerHeight < document.body.offsetHeight) && ((window.innerHeight + window.scrollY) === offset) || window.scrollY === 0)
+		window.scrollBy(0, 20000);
+}
+
+// for fix string
+var entityMap = {"&": "&amp;","<": "&lt;",">": "&gt;",'"': '&quot;',"'": '&#39;',"/": '&#x2F;'};
+
+function fix(string) {
+	return String(string).replace(/[&<>"'\/]/g, function (s) {
+		return entityMap[s];
+	});
+}
+
+// manage colour
+function change_color(hue, silent){
+
+	user_hue = hue;
+
+	colours = {
+		ninetyf: "hsl(" + (user_hue) + ", 100%, 90%)",
+		eighty: "hsl(" + (user_hue) + ", 100%, 80%)",
+		seventyf: "hsl(" + (user_hue) + ", 100%, 75%)",
+		seventy: "hsl(" + (user_hue) + ", 100%, 70%)",
+		sixty: "hsl(" + (user_hue) + ", 100%, 60%)",
+		fifty: "hsl(" + (user_hue) + ", 100%, 50%)",
+		fortyf: "hsl(" + (user_hue) + ", 100%, 45%)",
+		forty: "hsl(" + (user_hue) + ", 100%, 40%)",
+		thirty: "hsl(" + (user_hue) + ", 100%, 30%)",
+		twenty: "hsl(" + (user_hue) + ", 100%, 20%)"
+	};
+
+	$("form input").css({"background-color": colours.ninetyf, "color": idealTextColor(colours.ninetyf)}); //95
+	$("::selection, form button:hover").css({"background-color": colours.sixty}); //60
+	$("#float-logo, form button").css({"background-color": colours.fifty, "color": idealTextColor(colours.fifty)}); //50
+	$("#float-panel").css({"background-color": colours.fortyf, "color": idealTextColor(colours.fortyf)}); //45
+	$("#float-username").css({"background-color": colours.fortyf, "color": idealTextColor(colours.forty)}); //40
+	$("#input").css({"background-color": colours.twenty, "color": idealTextColor(colours.twenty)}); //20
+
+	// Text colour
+	$("#float-panel > h2, #users li, .version").css("color", idealTextColor(colours.fortyf));
+
+	// Logo colour
+	if($("#float-logo").css("color") === "rgb(245, 245, 245)")
+		$("#logo").attr('src', "/assets/bubbl.png")
+	else
+		$("#logo").attr('src', "/assets/bubbl_b.png")
+
+	socket.emit('change colour', user_hue);
+}
+
+// text colour functions
+function idealTextColor(bgColorhsl) {
+	bgColor = color2color(bgColorhsl, "hex");
+   	var nThreshold = 105;
+   	var components = getRGBComponents(bgColor);
+   	var bgDelta = (components.R * 0.299) + (components.G * 0.587) + (components.B * 0.114);
+
+   	return ((255 - bgDelta) < nThreshold) ? "#000000" : "#F5F5F5";   
+}
+
+function getRGBComponents(color) {       
+
+    var r = color.substring(1, 3);
+    var g = color.substring(3, 5);
+    var b = color.substring(5, 7);
+
+    return {
+       R: parseInt(r, 16),
+       G: parseInt(g, 16),
+       B: parseInt(b, 16)
+    };
+}
