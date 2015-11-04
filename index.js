cd1b4fca0b29c6e8d425c74658830ed61e227de0
lsp0 <liamsaliba@hotmail.com>
2015-11-04 23:06:58 +1100

    0.1.025c : Improved console logging (IP addresses, TODO: add IP blocking)

--- index.js
+++ index.js
@@ -26,22 +26,31 @@ io.on('connection', function(socket){  // listening socket
 		if(socket.userJoined) {
 			if(is_legal(msg) === 0){
 				clearTimeout(socket.inactiveTimeout);
-				socket.inactiveTimeout = setTimeout(function(){leave(socket.id);}, awayTime);
+				socket.inactiveTimeout = setTimeout(function(){leave();}, awayTime);
 				socket.broadcast.emit('chat message', {
 					username: socket.username,
 					colour: socket.colour,
 					message: fix(data.trim()), //nope... no XSS here
 				});
 				socket.prevMsg = msg;
+				print("m " + socket.id + " : " + socket.request.socket.remoteAddress + " : '" + msg.substring(0, 15) + "'");
 			}
-			else if(is_legal(msg) === 1)
-				io.to(socket.id).emit('error', "you need to type something to say something")
-			else if(is_legal(msg) === 2)
-				io.to(socket.id).emit('error', "you just said that")
-			else if(is_legal(msg) === 3)
-				io.to(socket.id).emit('error', "you're talking too fast")
-			else
-				io.to(socket.id).emit('error', "you sent something we couldn't handle")
+			else if(is_legal(msg) === 1) {
+				io.to(socket.id).emit('error', "you need to type something to say something");
+				print("e " + socket.id + " : " + socket.request.socket.remoteAddress + " : attempted blank message");
+			}
+			else if(is_legal(msg) === 2) {
+				io.to(socket.id).emit('error', "you just said that");
+				print("e " + socket.id + " : " + socket.request.socket.remoteAddress + " : attempted repeat message");
+			}
+			else if(is_legal(msg) === 3) {
+				io.to(socket.id).emit('error', "you're talking too fast");
+				print("e " + socket.id + " : " + socket.request.socket.remoteAddress + " : attempted fast message");
+			}
+			else {
+				io.to(socket.id).emit('error', "you sent something we couldn't handle");
+				print("e " + socket.id + " : " + socket.request.socket.remoteAddress + " : did something unknown");
+			}
 		}
 		socket.prevMsgTime = new Date();
 	});
@@ -49,7 +58,7 @@ io.on('connection', function(socket){  // listening socket
 	socket.on('join', function(){
 		if(!socket.userJoined){
 			clearTimeout(socket.inactiveTimeout);
-			socket.inactiveTimeout = setTimeout(function(){leave(socket.id);}, awayTime);
+			socket.inactiveTimeout = setTimeout(function(){leave();}, awayTime);
 
 			socket.username = genUser(); //store username in socket session for client
 			socket.colour = 280;
@@ -77,7 +86,7 @@ io.on('connection', function(socket){  // listening socket
 	});
 
 	socket.on('leave', function(){
-		leave(socket.id);
+		leave();
 	});
 
 	socket.on('rejoin', function(){
@@ -123,14 +132,13 @@ io.on('connection', function(socket){  // listening socket
 				success: false
 			});
 		}
-		socket.inactiveTimeout = setTimeout(function(){leave(socket.id);}, awayTime);
+		socket.inactiveTimeout = setTimeout(function(){leave();}, awayTime);
 	});
 	
 	socket.on('change colour', function(colour) {
 		if(socket.userJoined){
 			socket.colour = colour;
 			usernames[socket.id].colour = colour;
-			print("h " + socket.id + " : " + usernames[socket.id].username + " : h" + usernames[socket.id].colour);
 		}
 	});
 
@@ -142,12 +150,12 @@ io.on('connection', function(socket){  // listening socket
 
 	});
 
-	function leave(id){
+	function leave(){
 		if(socket.userJoined){
 			clearTimeout(socket.inactiveTimeout);
 
-			print("- " + id + " : " + socket.request.socket.remoteAddress);
-			delete usernames[id]
+			print("- " + socket.id + " : " + socket.request.socket.remoteAddress);
+			delete usernames[socket.id]
 
 			--users_active;
 			firstJoin = false;
@@ -164,15 +172,15 @@ io.on('connection', function(socket){  // listening socket
 	}
 
 	function is_legal(m){
-		if (m === "")
-			return 1;
-		else if (m === socket.prevMsg)
-			return 2;
-		else if (new Date() - socket.prevMsgTime < 1000)
-			return 3;
-		else
-			return 0;
-	}
+	if (m === "")
+		return 1;
+	else if (m === socket.prevMsg)
+		return 2;
+	else if (new Date() - socket.prevMsgTime < 1000)
+		return 3;
+	else
+		return 0;
+}
 });
 
 // HTTP server, listen for activity on port 3000
