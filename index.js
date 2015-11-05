ce5095480e8470de5198af4080444ef41393c7ac
lsp0 <liamsaliba@hotmail.com>
2015-11-05 18:24:06 +1100

    0.1.027 : Major server restructure
    prepare for monitoring client

--- index.js
+++ index.js
@@ -9,163 +9,151 @@ var io = require('socket.io')(http);
 // Route handler, sends index.html when root address is hit.
 app.use(express.static(__dirname + '/public'));
 
+// HTTP server, listen for activity on port 3000
+http.listen(8080, function(){
+	console.log("Public server started, listening on port 8080 (public: 80)");
+});
+
 // holds usernames
 var usernames = {};
-var users_active = 0;
+// count of users logged in: Object.keys(usernames).length
 
 // socket.io: user connection
+// TODO: rewrite a lot of this so most of the code is outside of io.on
 io.on('connection', function(socket){  // listening socket
 
 	var firstJoin = true;
-	var awayTime = 300000;
+	//usernames[socket.id] = {};
 
 	// TODO: IP Blocking
-
 
 	// emit chat message with username and message to other clients
 	socket.on('chat message', function(data){
 		msg = fix(data.trim());
-		if(socket.userJoined) {
-			if(is_legal(msg) === 0){
-				clearTimeout(socket.inactiveTimeout);
-				socket.inactiveTimeout = setTimeout(function(){leave();}, awayTime);
-				io.emit('chat message', {
-					username: socket.username,
-					colour: socket.colour,
-					message: fix(data.trim()), //nope... no XSS here
-				});
-				socket.prevMsg = msg;
-				print("m " + socket.id + " : " + socket.request.socket.remoteAddress + " : '" + msg.substring(0, 15) + "'");
-			}
-			else if(is_legal(msg) === 1) {
+		time = new Date();
+		if(usernames[socket.id].userJoined) {
+			if(msg === "") {
 				io.to(socket.id).emit('error', "you need to type something to say something");
-				print("e " + socket.id + " : " + socket.request.socket.remoteAddress + " : attempted blank message");
+				log('error', socket.id, "blank");
 			}
-			else if(is_legal(msg) === 2) {
+			else if(msg === usernames[socket.id].prevMsg && (time - usernames[socket.id].prevMsgTime < 4000)) {
 				io.to(socket.id).emit('error', "you just said that");
-				print("e " + socket.id + " : " + socket.request.socket.remoteAddress + " : attempted repeat message");
+				log('error', socket.id, "repeat");
 			}
-			else if(is_legal(msg) === 3) {
+			else if(time - usernames[socket.id].prevMsgTime < 1000) {
 				io.to(socket.id).emit('error', "you're talking too fast");
-				print("e " + socket.id + " : " + socket.request.socket.remoteAddress + " : attempted fast message");
+				log('error', socket.id, "fast");
 			}
 			else {
-				io.to(socket.id).emit('error', "you sent something we couldn't handle");
-				print("e " + socket.id + " : " + socket.request.socket.remoteAddress + " : did something unknown");
+				resetTimeout(true);
+				io.emit('chat message', {
+					username: usernames[socket.id].username,
+					colour: usernames[socket.id].colour,
+					message: msg //nope... no XSS here
+				});
+				usernames[socket.id].prevMsg = msg;
+				log('msg', socket.id, msg);
 			}
 		}
-		socket.prevMsgTime = new Date();
+		usernames[socket.id].prevMsgTime = time;
 	});
 
 	socket.on('join', function(){
-		if(!socket.userJoined){
-			clearTimeout(socket.inactiveTimeout);
-			socket.inactiveTimeout = setTimeout(function(){leave();}, awayTime);
+		if(typeof usernames[socket.id] === 'undefined'){
+			join();
 
-			socket.username = genUser(); //store username in socket session for client
-			socket.colour = 280;
-			socket.lastActive = new Date();
-			socket.userJoined = true;
-
-			usernames[socket.id] = {username: socket.username, colour: socket.colour, timeJoined: socket.lastActive, active: socket.active}; // store username in temporary db
-			print("+ " + socket.id + " : " + socket.request.socket.remoteAddress + " : " + usernames[socket.id].username + " : h" + usernames[socket.id].colour);
-			
-			++users_active;
-
-			socket.broadcast.emit('join', {
-				username: socket.username,
-				numUsers: users_active
-			});
-		
-			io.to(socket.id).emit("setname", {
-				username: socket.username,
-				numUsers: users_active,
-				success: true
-			});
 			if(firstJoin)
 				io.to(socket.id).emit("change available");
+
+			log('join', socket.id);
 		}
+	});
+
+	socket.on('rejoin', function(){
+		clearTimeout(usernames[socket.id].inactiveTimeout);
+		if ((Math.floor((new Date() - usernames[socket.id].timeJoined) / 60000) > 0 || firstJoin) && usernames[socket.id].userJoined) {
+			
+			socket.broadcast.emit('leave', {
+				username: usernames[socket.id].prevUsername,
+				numUsers: Object.keys(usernames).length
+			});
+
+			join();
+
+			firstJoin = false;
+			//msg to client when change username is available
+			setTimeout(function(){
+				io.to(socket.id).emit("change available");
+				log('avchange', socket.id);
+			}, 60000);
+
+			log('rejoin', socket.id);
+		}
+		else {
+			io.to(socket.id).emit("setname", {
+				username: usernames[socket.id].username,
+				numUsers: Object.keys(usernames).length,
+				success: false
+			});
+		}
+		usernames[socket.id].inactiveTimeout = setTimeout(function(){leave();}, 500000);
 	});
 
 	socket.on('leave', function(){
 		leave();
 	});
-
-	socket.on('rejoin', function(){
-		clearTimeout(socket.inactiveTimeout);
-		if ((Math.floor((new Date() - usernames[socket.id].timeJoined) / 60000) > 0 || firstJoin) && socket.userJoined) {
-			socket.prevUsername = socket.username;
-			socket.username = genUser();
-			socket.lastActive = new Date();
-			socket.userJoined = true;
-
-			firstJoin = false;
-
-			usernames[socket.id].username = socket.username;
-			usernames[socket.id].timeJoined = socket.lastActive;
-
-			socket.broadcast.emit('leave', {
-				username: socket.prevUsername,
-				numUsers: users_active
-			});
-			socket.broadcast.emit('join', {
-				username: socket.username,
-				numUsers: users_active
-			});
-
-			io.to(socket.id).emit("setname", {
-				username: socket.username,
-				numUsers: users_active,
-				success: true
-			});
-			print("* " + socket.id + " : " + socket.prevUsername + " -> " + usernames[socket.id].username);
-
-			//msg to client when change username is available
-			setTimeout(function(){
-				io.to(socket.id).emit("change available");
-				print("c " + socket.id)
-			}, 60000);
-		}
-		else {
-			print("* " + socket.id + " : " + usernames[socket.id].username + " : FAIL t" + Math.floor((new Date() - usernames[socket.id].timeJoined) / 60000));
-			io.to(socket.id).emit("setname", {
-				username: socket.username,
-				numUsers: users_active,
-				success: false
-			});
-		}
-		socket.inactiveTimeout = setTimeout(function(){leave();}, awayTime);
-	});
 	
 	socket.on('change colour', function(colour) {
-		if(socket.userJoined){
-			socket.colour = colour;
+		if(usernames[socket.id].userJoined)
 			usernames[socket.id].colour = colour;
-		}
 	});
 
 	socket.on('typing', function(msg){
 		//socket.broadcast.emit('back', socket.username); //TODO
+		resetTimeout(true);
 	});
 
 	socket.on('typing stop', function(){
 
 	});
 
+	function join(){
+		usernames[socket.id] = {
+			username: genUser(),
+			colour: 280,
+			timeJoined: new Date(),
+			userJoined: true,
+			ip: socket.request.socket.remoteAddress
+		};
+		resetTimeout(true);
+
+		socket.broadcast.emit('join', {
+			username: usernames[socket.id].username,
+			numUsers: Object.keys(usernames).length
+		});
+	
+		io.to(socket.id).emit("setname", {
+			username: usernames[socket.id].username,
+			numUsers: Object.keys(usernames).length,
+			success: true
+		});
+	}
+
+	// probably put these log statements in context.
+
 	function leave(){
-		if(socket.userJoined){
-			clearTimeout(socket.inactiveTimeout);
+		if(usernames[socket.id].userJoined){
+			log('leave', socket.id);
 
-			print("- " + socket.id + " : " + socket.request.socket.remoteAddress);
-			delete usernames[socket.id]
+			resetTimeout(false);
 
-			--users_active;
-			firstJoin = false;
+			delete usernames[socket.id];
+
 			socket.userJoined = false;
 
 			socket.broadcast.emit('leave', {
 				username: socket.username,
-				numUsers: users_active
+				numUsers: Object.keys(usernames).length
 			});
 
 			io.to(socket.id).emit("inactive");
@@ -173,28 +161,12 @@ io.on('connection', function(socket){  // listening socket
 		}
 	}
 
-	function is_legal(m){
-		if (m === "")
-			return 1;
-		else if (m === socket.prevMsg && (new Date() - socket.prevMsgTime < 4000))
-			return 2;
-		else if (new Date() - socket.prevMsgTime < 1000)
-			return 3;
-		else
-			return 0;
+	function resetTimeout(set) {
+		clearTimeout(usernames[socket.id].inactiveTimeout);
+		if(set)
+			usernames[socket.id].inactiveTimeout = setTimeout(function(){leave();}, 500000);
 	}
 });
-
-// HTTP server, listen for activity on port 3000
-http.listen(8080, function(){
-	print("Server initialised.")
-	print("Listening on *:8080")
-});
-
-function print(str){
-	var d = new Date();
-	console.log(d.toISOString() + ' | ' + str);
-}
 
 // generate username : TODO
 function genUser() {
@@ -209,7 +181,6 @@ function genUser() {
 	return name.join('');
 }
 
-
 // message legitimacy functions
 function fix(string) {
 	var entityMap = {"&": "&amp;","<": "&lt;",">": "&gt;",'"': '&quot;',"'": '&#39;',"/": '&#x2F;'};
@@ -223,4 +194,31 @@ function fix(string) {
 	str = str.replace(/(?:\r\n|\r|\n)/g, '<br />');
 	// rich text
 	return str;
+}
+
+
+//////// MONITORING /////////
+var mapp = express();
+var mhttp = require('http').Server(mapp);
+var mio = require('socket.io')(mhttp);
+
+mapp.use(express.static(__dirname + '/monitor'));
+
+mhttp.listen(3000, function(){
+	console.log("Monitoring server started, listening on port 3000");
+});
+
+
+mio.on('connection', function(socket){
+	setInterval(function() {
+		socket.volatile.emit(usernames);
+	}, 1000);
+});
+
+function log(id, user, data){
+	mio.sockets.emit(id, {
+		user: user,
+		data: data,
+		time: new Date()
+	});
 }
